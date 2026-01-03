a8a838dd3bdb4feec71c2d73470ee963162db9e0
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2026-01-03 15:11:51 +0530

    Changes to call backend service get result and display

--- App/FrontEnd/react-app/src/App.js
+++ App/FrontEnd/react-app/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+//import axios from "axios" to make HTTP requests to the backend API
+//axios are generally used in enterprise-level applications for its advanced features
+import React, { useEffect, useState } from "react";
+//import React is the core library for building user interfaces
+//import useEffect and useState are React hooks for managing side effects and state
 function App() {
+  // useState hook to manage the message state
+  // message will hold the data fetched from the backend API
+  // setMessage is the function to update the message state
+  const [message, setMessage] = useState("");
+
+  // useEffect hook to fetch data from the backend API when the component mounts
+  // fetch is used to make an HTTP GET request to the specified URL
+  // fetch is browser-native and does not require an additional library
+  // fetch example:
+  /*useEffect(() => {
+    fetch("https://localhost:7211/api/hello")
+      .then((response) => response.text()) // backend returns plain string
+      .then((data) => setMessage(data))
+      .catch((error) => console.error("Error fetching API:", error));
+  }, []);*/
+
+  //axios is a promise-based HTTP client for the browser and Node.js
+  useEffect(() =>{
+    axios.get("https://localhost:7211/api/hello")
+      .then((response) => {
+        setMessage(response.data); // backend returns plain string
+      })
+      .catch((error) => {
+        console.error("Error fetching API:", error);
+      });
+    }, []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Frontend</h1>
+      <p>{message}</p>
     </div>
   );
 }

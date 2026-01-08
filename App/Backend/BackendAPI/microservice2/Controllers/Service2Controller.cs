9dbe673d3b714456a740babfa7ad0e06c6f88362
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2026-01-08 08:03:11 +0530

    Created 2nd microservice

---
+++ App/Backend/BackendAPI/microservice2/Controllers/Service2Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace microservice1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Service2Controller : ControllerBase
+    {
+
+        [HttpGet("message")]
+        public IActionResult GetMessage()
+        {
+            string message = "Hello from Microservice 2!";
+            return Ok(message);
+        }
+
+
+        [HttpGet("health")]
+        public IActionResult health()
+        {
+            return Ok("Microservice 2 working fine");
+        }
+    }
+}

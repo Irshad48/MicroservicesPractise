f344669d686ebf1f4c827359dbaa83ac659fae8d
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2026-01-08 08:46:35 +0530

    Sync API call - improved version - return entire response - success check - still not recommended due to httpClient

--- App/Backend/BackendAPI/microservice1/Controllers/Service1Controller.cs
+++ App/Backend/BackendAPI/microservice1/Controllers/Service1Controller.cs
@@ -9,13 +9,26 @@ namespace microservice1.Controllers
     {
 
         [HttpGet("message")]
-        public IActionResult GetMessage()
+        public async Task<IActionResult> GetMessage()
         {
             // Simplest way to call another microservice
             // In production, consider using HttpClientFactory or other robust methods
             // this is not recommended for production code due to potential socket exhaustion issues
-            var httpClient = new HttpClient();
+
+            /*var httpClient = new HttpClient();
             string messageFromService2 = httpClient.GetStringAsync("https://localhost:7132/api/service2/message").Result;
+            string message = $"Hello from Microservice 1! \n{messageFromService2}";*/
+
+            // Improved version with error handling
+            // using async/await - remember to add 'async' to method signature - also change return type to Task<IActionResult> because async returns a Task     
+
+            var httpClient = new HttpClient();
+            var httpResponse = await httpClient.GetAsync("https://localhost:7132/api/service2/message");
+            if(!httpResponse.IsSuccessStatusCode)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error calling Microservice 2");
+            }
+            string messageFromService2 = await httpResponse.Content.ReadAsStringAsync();
             string message = $"Hello from Microservice 1! \n{messageFromService2}";
 
             return Ok(message);

15e7259f4fb81dea7bed0ac33541e97c8a9fea17
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2026-01-08 08:31:09 +0530

    Synchronous API call - Simplest way - not recommended

--- App/Backend/BackendAPI/microservice1/Controllers/Service1Controller.cs
+++ App/Backend/BackendAPI/microservice1/Controllers/Service1Controller.cs
@@ -11,7 +11,13 @@ namespace microservice1.Controllers
         [HttpGet("message")]
         public IActionResult GetMessage()
         {
-            string message = "Hello from Microservice 1!";
+            // Simplest way to call another microservice
+            // In production, consider using HttpClientFactory or other robust methods
+            // this is not recommended for production code due to potential socket exhaustion issues
+            var httpClient = new HttpClient();
+            string messageFromService2 = httpClient.GetStringAsync("https://localhost:7132/api/service2/message").Result;
+            string message = $"Hello from Microservice 1! \n{messageFromService2}";
+
             return Ok(message);
         }
 
